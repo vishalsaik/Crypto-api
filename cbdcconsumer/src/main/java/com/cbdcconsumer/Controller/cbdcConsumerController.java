3ea3f3b362e08a5116adf0e69173f42425036593
vishalsaik <saivishal003@gmail.com>
2026-01-21 16:30:17 +0530

    Added Load balancer

--- cbdcconsumer/src/main/java/com/cbdcconsumer/Controller/cbdcConsumerController.java
+++ cbdcconsumer/src/main/java/com/cbdcconsumer/Controller/cbdcConsumerController.java
@@ -2,6 +2,9 @@ package com.cbdcconsumer.Controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,31 +19,47 @@ import com.cbdcconsumer.dto.Token;
 @RestController
 @RequestMapping("api/v1/cbdcconsumer")
 public class cbdcConsumerController {
+	
+	@Autowired
+	DiscoveryClient ds;
+	@Autowired
+	RestTemplate rTemp;
 	@GetMapping
 	public List<Token> getAllToken(){
-		
-		RestTemplate rTemp = new RestTemplate();
-		List<Token> tt = rTemp.getForObject("http://localhost:10000/api/v1/cbdc", List.class);
+//		List<ServiceInstance>ss=ds.getInstances("CBDC");
+//		ServiceInstance s1 =ss.get(0);
+//		String url=s1.getUri().toString();
+//		RestTemplate rTemp = new RestTemplate();
+		List<Token> tt = rTemp.getForObject("http://CBDC/api/v1/cbdc", List.class);
 		
 		return tt;
 		
 	}
 	@PostMapping
 	public String createToken(@RequestBody Token tk){
-		RestTemplate rTemp = new RestTemplate();
-		Token tt = rTemp.postForObject("http://localhost:10000/api/v1/cbdc",tk,Token.class);
+//		List<ServiceInstance>ss=ds.getInstances("CBDC");
+//		ServiceInstance s1 =ss.get(0);
+//		String url=s1.getUri().toString();
+//		RestTemplate rTemp = new RestTemplate();
+		Token tt = rTemp.postForObject("http://CBDC/api/v1/cbdc",tk,Token.class);
 		return "Ok created";
 	}
 	@PutMapping("{i}")
 	public String updateToken(@PathVariable("i") long id,@RequestBody Token tk){
-		RestTemplate rTemp = new RestTemplate();
-		Token tt = rTemp.postForObject("http://localhost:10000/api/v1/cbdc",tk,Token.class);
+//		List<ServiceInstance>ss=ds.getInstances("CBDC");
+//		ServiceInstance s1 =ss.get(0);
+//		String url=s1.getUri().toString();
+//		RestTemplate rTemp = new RestTemplate();
+		Token tt = rTemp.postForObject("http://CBDC/api/v1/cbdc",tk,Token.class);
 		return "Ok updated";
 	}
 	@GetMapping("{i}")
 	public Token getToken(@PathVariable("i") long id){
-		RestTemplate rTemp = new RestTemplate();
-		Token tt = rTemp.getForObject("http://localhost:10000/api/v1/cbdc/"+id,Token.class);
+//		List<ServiceInstance>ss=ds.getInstances("CBDC");
+//		ServiceInstance s1 =ss.get(0);
+//		String url=s1.getUri().toString();
+//		RestTemplate rTemp = new RestTemplate();
+		Token tt = rTemp.getForObject("http://CBDC/api/v1/cbdc/"+id,Token.class);
 		return tt;
 	}
 	
